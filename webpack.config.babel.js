17614c96fb2965298724a3470e16902522b74794
Bernard McManus <bernard.mcmanus.iv@gmail.com>
2017-02-25 13:31:50 -0500

    add webpack script

---
+++ webpack.config.babel.js
@@ -0,0 +1,35 @@
+import { BannerPlugin, optimize } from 'webpack';
+import pkg from './package.json';
+
+const banner = `${pkg.name} - ${pkg.version} - ${new Date().toISOString()}`;
+
+export default {
+	context: __dirname,
+	entry: './lib/index.js',
+	output: {
+		path: 'build',
+		library: pkg.name,
+		libraryTarget: 'umd',
+		filename: `${pkg.name}.js`
+	},
+	module: {
+		loaders: [{
+			test: /package.json$/,
+			exclude: /node_modules/,
+			loader: 'null-loader'
+		}, {
+			test: /\.js$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/
+		}, {
+			test: /\.json$/,
+			loader: 'json-loader'
+		}]
+	},
+	plugins: [
+		new BannerPlugin({ banner, entryOnly: true }),
+		new optimize.UglifyJsPlugin({
+			compress: { warnings: false }
+		})
+	]
+};
